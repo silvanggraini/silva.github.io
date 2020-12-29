56f27727c27c3149529fabd9f3df3c4ddc276baa
yaya <55609403+Felia-Rhafida@users.noreply.github.com>
2020-12-29 15:27:20 +0700

    Add files via upload

--- pweb_UTS/signUp.js
+++ pweb_UTS/signUp.js
@@ -26,17 +26,20 @@ email.addEventListener('keyup',function(){
     }
 })
 
+var apalah  = document.querySelector(".getStarted")
 var password = document.querySelector('#password');
 password.addEventListener('keyup',function(){
     var passKosong = document.getElementById('passKosong');
     if (password.value.length == 0 || password.value.length < 8) {
         password.style.border = "1px solid red";
         passKosong.style.display = "block";
+        apalah.setAttribute('href','#');
         return false;
     }
     else{
         password.style.border = "1px solid #40a8c4";
         passKosong.style.display = "none";
+        apalah.setAttribute('href','login.html')
     }
 })
 
